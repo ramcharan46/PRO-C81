f2a5938a9acd895da6b8859b727f2194a2a12a86
ramcharan46 <68854565+ramcharan46@users.noreply.github.com>
2021-01-14 18:44:18 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,3 @@
+# C81-Project
+
+Output link: https://drive.google.com/drive/folders/1-FqSicQM2RoHcx3SyP5INyGFMnGlsTBH?usp=sharing
---
+++ components/AppDrawerNavigator.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {createDrawerNavigator} from 'react-navigation-drawer';
+import {AppTabNavigator} from './AppTabNavigator';
+import CustomSidebarMenu from './CustomSidebarMenu';
+import SettingScreen from '../screens/SettingsScreen';
+
+export const AppDrawerNavigator =  createDrawerNavigator({
+    Home: {
+        screen: AppTabNavigator
+    },
+    Setting: {
+        screen: SettingScreen
+    }
+    },
+
+    {contentComponent: CustomSidebarMenu},
+    
+    {
+        initialRouteName: 'Home'
+    }
+);
